a7a90b8f5b1faf056decb120ae202309ddb602a4
Shailaja362 <shailajasankar479@gmail.com>
2025-12-03 13:03:44 +0530

    Input Field Hover colour code changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,7 @@
                                 <div class="relative">
                                     <input :type="show ? 'text' : 'password'" name="password"
                                         class="w-full border border-gray-300 rounded-md px-3 py-2 mt-1
-                              focus:outline-none focus:ring-2 focus:ring-[#ab5f00] pr-10"
+                             focus:outline-none focus:ring focus:ring-primary/40 pr-10"
                                         placeholder="••••••••" />
                                     <button type="button" @click="show = !show"
                                         class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 text-lg">
