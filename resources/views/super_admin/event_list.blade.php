af8eb9644d1dd311749660363d527a7fe39068a9
Shailaja362 <shailajasankar479@gmail.com>
2026-07-27 10:26:37 +0530

    updated

--- resources/views/super_admin/event_list.blade.php
+++ resources/views/super_admin/event_list.blade.php
@@ -208,12 +208,12 @@
                                         class="text-blue-600 hover:text-blue-800">
                                         <i class="fa-solid fa-pen-to-square"></i>
                                     </a>
-                                    <button type="button"
+                                    {{-- <button type="button"
                                         class="togglePublish {{ $event->publish ? 'text-yellow-600 hover:text-yellow-800' : 'text-gray-500 hover:text-gray-700' }} mx-2"
                                         data-id="{{ $event->id }}" data-publish="{{ $event->publish }}"
                                         title="{{ $event->publish ? 'Unpublish Event' : 'Publish Event' }}">
                                         <i class="fa-solid {{ $event->publish ? 'fa-eye' : 'fa-eye-slash' }}"></i>
-                                    </button>
+                                    </button> --}}
                                     @php
                                         $canDelete = $event->schedules->every(function ($schedule) {
                                             return $schedule->registrations->isEmpty();
