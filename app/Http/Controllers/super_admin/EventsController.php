9ab57a0e84e7ab7b8b3abe60039b0f605b736617
Shailaja362 <shailajasankar479@gmail.com>
2026-09-04 09:47:01 +0530

    updated code

--- app/Http/Controllers/super_admin/EventsController.php
+++ app/Http/Controllers/super_admin/EventsController.php
@@ -127,10 +127,6 @@ class EventsController extends Controller
         if ($request->event_id) {
             $eventId = decrypt($request->event_id);
             $this->data['edit_event'] = Event::where('id', $eventId)->first();
-            if ($this->data['edit_event'] && $this->data['edit_event']->publish) {
-                return redirect()->route('event_list')
-                    ->with('error', 'A published event cannot be edited. Unpublish it first.');
-            }
             $this->data['edit_faculty'] = Faculty::where('id', $this->data['edit_event']->faculty_id)->first();
         } else {
             $this->data['edit_event'] = null;
@@ -243,12 +239,6 @@ class EventsController extends Controller
             $isNewEvent = empty($request['event_id']);
             if (!$isNewEvent) {
                 $event = Event::find($request['event_id']);
-                if ($event && $event->publish) {
-                    return response()->json([
-                        'success' => false,
-                        'message' => 'A published event cannot be edited. Unpublish it first.',
-                    ], 422);
-                }
                 $message = 'Event Updated successfully';
             } else {
                 $event = new Event();
--- resources/views/super_admin/event_list.blade.php
+++ resources/views/super_admin/event_list.blade.php
@@ -211,16 +211,10 @@
                                 </td>
                                 <!-- Action -->
                                 <td class="px-3 py-2 text-center">
-                                    @if ($event->publish)
-                                        <span class="text-gray-400 cursor-not-allowed" title="Published events cannot be edited">
-                                            <i class="fa-solid fa-pen-to-square"></i>
-                                        </span>
-                                    @else
-                                        <a href="{{ route('create_event', ['event_id' => encrypt($event->id)]) }}"
-                                            class="text-blue-600 hover:text-blue-800">
-                                            <i class="fa-solid fa-pen-to-square"></i>
-                                        </a>
-                                    @endif
+                                    <a href="{{ route('create_event', ['event_id' => encrypt($event->id)]) }}"
+                                        class="text-blue-600 hover:text-blue-800">
+                                        <i class="fa-solid fa-pen-to-square"></i>
+                                    </a>
                                     {{-- <button type="button"
                                         class="togglePublish {{ $event->publish ? 'text-yellow-600 hover:text-yellow-800' : 'text-gray-500 hover:text-gray-700' }} mx-2"
                                         data-id="{{ $event->id }}" data-publish="{{ $event->publish }}"
